1d5a9f2fd314886ce8994c92c18c09db653f0d39
Abdul Dakkak <abduld@wolfram.com>
2016-03-10 22:25:22 -0600

    tweaks

--- cmd/build/opendocument.go
+++ cmd/build/opendocument.go
@@ -13,7 +13,14 @@ import (
 	"gitlab.com/abduld/wgx-pandoc/pkg"
 )
 
+const docxCopyright = `
+------
+
+This work is licensed by UIUC and NVIDIA (2016) under a Creative Commons Attribution-NonCommercial 4.0 License.
+`
+
 func OpenDocument(outputDir, cmakeFile string, progress *pb.ProgressBar) (string, error) {
+
 	doc, err := makeDoc(outputDir, cmakeFile, progress)
 	if err != nil {
 		return "", err
@@ -31,8 +38,8 @@ func OpenDocument(outputDir, cmakeFile string, progress *pb.ProgressBar) (string
 	}
 	incrementProgress(progress)
 
-	progressPostfix(progress, "Writing Markdown file...")
-	outFile := filepath.Join(outputDir, "Module["+strconv.Itoa(doc.Module)+"]-"+doc.FileName+".docx")
+	progressPostfix(progress, "Writing ODT file...")
+	outFile := filepath.Join(outputDir, "Module["+strconv.Itoa(doc.Module)+"]-"+doc.FileName+".odt")
 
 	tmpDir := os.TempDir()
 	tmpOutFile := filepath.Join(tmpDir, "wgx-pandoc-markdown.markdown")
@@ -44,8 +51,8 @@ func OpenDocument(outputDir, cmakeFile string, progress *pb.ProgressBar) (string
 		"-f",
 		pandoc.MarkdownFormat,
 		"-t",
-		"docx",
-		"--template=" + rtfTemplate.fileName,
+		"opendocument",
+		"--template=" + opendocumentTemplate.fileName,
 		"-o",
 		outFile,
 		tmpOutFile,
@@ -63,6 +70,36 @@ func OpenDocument(outputDir, cmakeFile string, progress *pb.ProgressBar) (string
 	}
 
 	incrementProgress(progress)
+
+	progressPostfix(progress, "Writing Docx file...")
+
+	tmpOutFile = filepath.Join(tmpDir, "wgx-pandoc-docx-markdown.markdown")
+	ioutil.WriteFile(tmpOutFile, []byte(document+docxCopyright), 0644)
+	outFile = filepath.Join(outputDir, "Module["+strconv.Itoa(doc.Module)+"]-"+doc.FileName+".docx")
+
+	args = []string{
+		"-s",
+		"-N",
+		"-f",
+		pandoc.MarkdownFormat,
+		"-t",
+		"docx",
+		"--template=" + opendocumentTemplate.fileName,
+		"-o",
+		outFile,
+		tmpOutFile,
+	}
+	args = append(args, pandoc.DefaultFilter...)
+	cmd = exec.Command("pandoc", args...)
+	cmd.Dir = tmpDir
+	out, err = cmd.CombinedOutput()
+	if len(out) > 0 {
+		ioutil.WriteFile(filepath.Join(outputDir, doc.FileName+".gen.docx.log"), out, 0644)
+	}
+	if err != nil {
+		progress.FinishPrint("✖ Failed to generate Docx file. Error :: " + err.Error())
+		return "", err
+	}
 
 	return outFile, nil
 }
